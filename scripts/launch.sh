e0d4dabcf9e1aaa76dc9548ab4abc4a8ceba6de5
Qiang Liu <cyruscyliu@gmail.com>
2025-04-22 10:50:54 +0200

    Revert "Reset core"
    This reverts commit 6bce4bcd360c217afc38d7f4cc848c99e147d279.

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -93,7 +93,6 @@ function single {
     container_id=$(cut -c-12 <<< $container_id)
     echo $container_id
     docker logs -f "$container_id" &> logs/$container.log &
-    core=""
 }
 
 function multiple {
